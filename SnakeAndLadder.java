fcc25b883910f4c88b3a791295c44ba66ce75706
aravindkarri <aravindkarri98495@gmail.com>
2021-06-22 14:41:01 +0530

    Added: Initialize the single player at start position zero

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -5,6 +5,7 @@ public class SnakeAndLadder {
 	public static void main(String args[]) {
 		
 		System.out.println("Welcome to Snake and Ladder Simulation");
+		int playerPosition = 0;  //Single player at start position zero
 		
 	}
 }
